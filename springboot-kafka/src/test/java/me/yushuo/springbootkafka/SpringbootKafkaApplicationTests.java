586d994447a411acaea3f76e184f4d75d9eb5d2e
ys <secretys123@gmail.com>
2020-03-11 11:04:38 +0800

    add springboot-kafka

---
+++ springboot-kafka/src/test/java/me/yushuo/springbootkafka/SpringbootKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package me.yushuo.springbootkafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
